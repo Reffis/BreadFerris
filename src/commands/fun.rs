90ddbd6bbdc74ec249ac2107d30d04cc8165af4f
fn79 <color0470@gmail.com>
2021-08-10 13:41:20 +0900

    Add `nitro`, `choice` Command

---
+++ src/commands/fun.rs
@@ -0,0 +1,35 @@
+use breadferris::cmdlog;
+use serenity::framework::standard::{macros::command, CommandResult, Args};
+use serenity::model::prelude::*;
+use serenity::prelude::*;
+use rand::prelude::{SliceRandom};
+
+#[command]
+#[aliases("랜덤", "골라", "random")]
+async fn choice(ctx: &Context, msg: &Message, args: Args) -> CommandResult {
+    let r = args.rest().split(", ").collect::<Vec<_>>();
+    cmdlog(msg.author.id.to_string(), msg.content.clone());
+    msg.reply(ctx, format!("`{}`", r.choose(&mut rand::thread_rng()).unwrap_or_else(|| &""))).await?;
+    Ok(())
+}
+
+#[command]
+#[aliases("니트로")]
+async fn nitro(ctx: &Context, msg: &Message) -> CommandResult {
+    let mut v = String::new();
+    (0..16).enumerate().for_each(|_| {
+        v.push(
+            *({
+                let mut _v = ('a'..='z').collect::<Vec<_>>();
+                _v.append(&mut ('A'..='Z').collect::<Vec<_>>());
+                _v
+            }
+                .choose(&mut rand::thread_rng()))
+                .unwrap_or_else(|| &' '),
+        );
+    });
+    msg.reply(ctx, format!("`{}`", v)).await?;
+
+    cmdlog(msg.author.id.to_string(), msg.content.clone());
+    Ok(())
+}
