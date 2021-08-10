0eb9d1762303025686c5cc6edb3f3cb7eca9d3d3
fn79 <color0470@gmail.com>
2021-08-10 11:18:50 +0900

    Add moderator

--- src/commands/mod.rs
+++ src/commands/mod.rs
@@ -5,12 +5,14 @@ image: 다양한 api를 이용하여, 이미지를 가져오는 명령어를 모
 owner: 봇 주인만 쓸 수 있는 명령어를 모아둔 카테고리입니다.
 util: 사실상, 메인 명령어가 있는 카테고리입니다.
 other: 어떤 카테고리에 넣을지 모르거나, 애매한 명령어를 모아둔 카테고리입니다.
+moderator: 서버의 관리자들이 사용할 수 있는 명령어(킥, 밴, 언밴 등)를 모아둔 카테고리입니다.
 */
 
 pub mod image;
 pub mod other;
 pub mod owner;
 pub mod util;
+pub mod moderator;
 
 /*
 owner 카테고리에 있는 eval과 util 카테고리에 있는 run 의 차이점:
