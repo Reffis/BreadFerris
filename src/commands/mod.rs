6547b790ae3a59788cabbef4f4629d78f5a88581
fn79 <color0470@gmail.com>
2021-08-12 00:11:10 +0900

    Add `avatar` Command

--- src/commands/mod.rs
+++ src/commands/mod.rs
@@ -44,7 +44,7 @@ pub const HELP_MODER: &str = r#"
 "#;
 
 pub const HELP_FUN: &str = r#"
-> `nitro`, `choice [item, item, ...]`, `bce [Text]` `say [Text]`
+> `nitro`, `choice [item, item, ...]`, `bce [Text]` `say [Text]` `avatar [mention or id]`
 "#;
 
 /*
