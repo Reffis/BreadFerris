bc8e45a0a669b9d62db801982e4f5bd228d4e77d
fn79 <color0470@gmail.com>
2021-08-16 09:37:19 +0900

    Update `dev` Command

--- src/commands/util.rs
+++ src/commands/util.rs
@@ -228,7 +228,7 @@ async fn dev(ctx: &Context, msg: &Message) -> CommandResult {
                         r#"
 > **Rust Version:** cargo 1.54.0 (2021-06-22)
 
-> **Editor:** IntelliJ ~~(or VSCode)~~
+> **Editor:** Visual Studio Code
 
 > **OS:** Windows 10 - 20H2 (OS Build 19042.1110)
 
