bd0851ca0d25936d184bb37f81ca225910159b03
fn79 <color0470@gmail.com>
2021-08-20 19:26:19 +0900

    Add `gunghab` Command

--- src/main.rs
+++ src/main.rs
@@ -58,7 +58,7 @@ struct Other;
 struct Moderator;
 
 #[group]
-#[commands(choice, nitro, say, avatar, hangang, bbangcat)]
+#[commands(choice, nitro, say, avatar, hangang, bbangcat, gunghab)]
 struct Fun;
 
 #[tokio::main]
