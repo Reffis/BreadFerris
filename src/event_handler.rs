7c0c7cc92e238a9170b26370c54ba476eb17e9ec
fn79 <color0470@gmail.com>
2021-08-20 23:57:42 +0900

    Update `event_handler.rs`

--- src/event_handler.rs
+++ src/event_handler.rs
@@ -1,10 +1,13 @@
 use breadferris::{log, LogType::*};
+use serenity::model::channel::Message;
 use serenity::model::gateway::Activity;
 use serenity::{
     async_trait,
     model::{event::ResumedEvent, gateway::Ready},
     prelude::*,
 };
+
+use super::commands::embed_colors::*;
 
 pub struct Handler;
 
@@ -21,5 +24,32 @@ impl EventHandler for Handler {
 
     async fn resume(&self, _: Context, _: ResumedEvent) {
         log(Info, "Resumed".to_string());
+    }
+
+    async fn message(&self, ctx: Context, msg: Message) {
+        if msg.author.bot {
+            return;
+        }
+        if msg.content.to_lowercase() == "ferris" {
+            if let Err(_) = msg.channel_id.send_message(&ctx.http, |m| {
+                m.embed(|embed| {
+                    embed.colour(random_color())
+                    .title("Ferris Bot")
+                    .description(r#"
+**페리스 봇입니다!**
+
+도움말은 `ferris help` 를 입력하여, 확인해주세요.
+
+또한, 페리스 봇의 모든것은 오픈소스입니다!
+(`ferris help` 로 확인해주세요.)
+
+**- `! Bread Cat#0002` -**
+                    "#)
+                    .thumbnail("https://cdn.discordapp.com/attachments/850930041487622197/878290746912960542/bot.png")
+                })
+            }).await {
+                log(Error, "ferris / Failed to send message".to_string());
+            }
+        }
     }
 }
