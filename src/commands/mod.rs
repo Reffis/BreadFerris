9894b86d640588a43ae99b2ce6636057ef061360
fn79 <color0470@gmail.com>
2021-08-27 18:29:43 +0900

    Add `spoiler` Command

--- src/commands/mod.rs
+++ src/commands/mod.rs
@@ -50,7 +50,7 @@ pub const HELP_MODER: &str = r#"
 "#;
 
 pub const HELP_FUN: &str = r#"
-> `nitro`, `choice [item, item, ...]`, `say [Text]`, `avatar [mention or id]`, `hangang`, `bbangcat`, `gunghab`, `owo`
+> `nitro`, `choice [item, item, ...]`, `say [Text]`, `avatar [mention or id]`, `hangang`, `bbangcat`, `gunghab [user1] [user2]`, `owo`, `spoiler [Text]`
 "#;
 
 /*
