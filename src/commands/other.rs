0b57ba5ffab095fa70a2af83efb49b010c55a763
fn79 <color0470@gmail.com>
2021-08-30 09:44:24 +0900

    Update `Log` (`other` All Commands)

--- src/commands/other.rs
+++ src/commands/other.rs
@@ -1,12 +1,11 @@
+use bbanglog::info;
 use serenity::framework::standard::{macros::command, CommandResult};
 use serenity::model::prelude::*;
 use serenity::prelude::*;
 
-use breadferris::cmdlog;
-
 #[command]
 #[aliases("샍", "샌즈", "샍즈", "샌주", "샍주")]
 async fn sans(_ctx: &Context, msg: &Message) -> CommandResult {
-    cmdlog(&msg.author.id, &msg.content);
+    info!("Command: [{}] {}", &msg.author.id, &msg.content);
     Ok(())
 }
