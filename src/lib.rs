e29f90d8d7851ee3b0672ebea548f9987478529d
fn79 <color0470@gmail.com>
2021-09-02 12:10:10 +0900

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 use serde_derive::Deserialize;
 use std::{fmt::Debug, io::Read};
-
 #[derive(Debug, Deserialize)]
+
 pub struct Config {
     config: Option<BotConfig>,
 }
@@ -9,6 +9,7 @@ pub struct Config {
 #[derive(Debug, Deserialize)]
 pub struct BotConfig {
     pub token: Option<String>,
+
     pub prefix: Option<Vec<String>>,
     pub support_channel: Option<u64>,
     pub application_id: Option<u64>,
@@ -18,7 +19,6 @@ pub fn loadconfig() -> BotConfig {
     let mut file = std::fs::File::open("config/config.toml").unwrap();
     let mut c = String::new();
     file.read_to_string(&mut c).unwrap();
-
     let decoded: Config = toml::from_str(c.as_str()).unwrap();
     decoded.config.unwrap()
 }
